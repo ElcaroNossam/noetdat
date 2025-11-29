afb5090e6661ace3e31ad802982010c88c64d504
illia.teslenko <illia.teslenko@onix-systems.com>
2025-11-29 01:22:33 +0100

    update style

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",  # Локализация
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -56,6 +57,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
             ],
         },
     },
@@ -93,13 +95,26 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
 TIME_ZONE = "UTC"
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
+
+# Поддерживаемые языки
+LANGUAGES = [
+    ('ru', 'Русский'),
+    ('en', 'English'),
+    ('es', 'Español'),
+    ('he', 'עברית'),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 
 
 STATIC_URL = "/static/"
--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("i18n/setlang/", set_language, name="set_language"),
+]
+
+urlpatterns += i18n_patterns(
     path("accounts/", include(("accounts.urls", "accounts"), namespace="accounts")),
     path("api/", include(("api.urls", "api"), namespace="api")),
     path("alerts/", include(("alerts.urls", "alerts"), namespace="alerts")),
     path("", include(("screener.urls", "screener"), namespace="screener")),
-]
+    prefix_default_language=False,
+)
 
 
